d3cd500667b0fe6397a7570e98386379c4819ebc
Alaa Emad <www.logical123@gmail.com>
2024-04-29 07:57:18 +0300

    settings update hosts

--- src/tourism/settings.py
+++ src/tourism/settings.py
@@ -21,8 +21,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["10.0.2.2","127.0.0.1"]
 
 
 # Application definition
