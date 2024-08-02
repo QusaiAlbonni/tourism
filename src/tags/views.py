2166e4520e2e5fc30f7449cecc79a81439297f6e
Alaa emad <www.logical123@gmail.com>
2024-08-02 14:27:09 +0300

    tags for filter

--- src/tags/views.py
+++ src/tags/views.py
@@ -3,23 +3,44 @@ from .models import TagsCategory, Tag
 from .serializers import TagsCategorySerializer, TagSerializer
 from app_auth.permissions import isAdminOrReadOnly
 from rest_framework.permissions import IsAuthenticated
-    
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class TagsCategoryViewSet(viewsets.ModelViewSet):
     queryset = TagsCategory.objects.all()
     serializer_class = TagsCategorySerializer
-    # permission_classes = [IsAuthenticated,isAdminOrReadOnly]
+    permission_classes = [IsAuthenticated,isAdminOrReadOnly]
     http_method_names = ["get", "post", "delete", "head", "options", "trace"]
     def get_queryset(self):
         type = self.request.query_params.get('type')
         if type:
             return self.queryset.filter(type=type)
         return self.queryset.all()
+    
+    @action(detail=False, methods=['get'], url_path='by-type')
+    def get_categories_with_tags(self, request):
+        category_type = request.query_params.get('type')
+        if not category_type:
+            return Response({"error": "Type parameter is required."}, status=400)
+
+        categories = TagsCategory.objects.filter(type=category_type)
+        response_data = []
+
+        for category in categories:
+            tags = Tag.objects.filter(category=category)
+            tags_serializer = TagSerializer(tags, many=True)
+            response_data.append({
+                "category": TagsCategorySerializer(category).data,
+                "tags": tags_serializer.data
+            })
+
+        return Response(response_data)
+    
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
-    # permission_classes = [IsAuthenticated,isAdminOrReadOnly]
+    permission_classes = [IsAuthenticated,isAdminOrReadOnly]
     http_method_names = ["get", "post", "delete", "head", "options", "trace"]
 
     def get_queryset(self):
